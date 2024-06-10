5b702a8c3e598eb2a606615b9aca60fa43b391c8
DemenNgoding <alfianeff123@gmail.com>
2024-06-10 22:25:53 +0700

    add attachment

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -3,6 +3,12 @@ defineProps({
     post: Object
 })
 
+// this function is to check
+function checkImage(attachment) {
+    const mime = attachment.mime.split('/')
+    return mime[0].toLowerCase() === 'image'
+}
+
 </script>
 
 <template>
@@ -24,6 +30,24 @@ defineProps({
 
         <!--        caption-->
         <div v-html="post.body"></div>
+
+        <div class="grid grid-cols-2 gap-3">
+            <div v-for="attachment of post.attachment">
+                <img v-if="checkImage(attachment)" :src="attachment.url">
+            </div>
+        </div>
+
+        <div>
+            <button
+                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Like
+            </button>
+
+            <button
+                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                Comment
+            </button>
+        </div>
     </div>
 </template>
 
