f3698e9db0de1479acb0e2362f1a798b31381929
Calebfelix0125 <nathanaelfelix1009@gmail.com>
2024-06-15 21:55:41 +0700

    Show Profile with Edit Profile

---
+++ resources/js/Pages/Profile/View.vue
@@ -0,0 +1,15 @@
+<script setup>
+import {usePage} from "@inertiajs/vue3";
+
+const user = usePage().props.auth.user;
+</script>
+
+<template>
+    <div>
+        <pre>{{ user }}</pre>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
