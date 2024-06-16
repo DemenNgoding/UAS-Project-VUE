ace19183a3f3aeb65d779e5ccf8a1f418526a5cd
Calebfelix0125 <nathanaelfelix1009@gmail.com>
2024-06-16 20:27:01 +0700

    Profile page with Edit Page Button

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,6 +5,9 @@ import CommunityList from "@/Components/app/CommunityList.vue";
 import SearchList from "@/Components/app/SearchList.vue";
 import CreatePost from "@/Components/app/CreatePost.vue";
 import PostList from "@/Components/app/PostList.vue";
+import {usePage} from "@inertiajs/vue3";
+
+const user = usePage().props.auth.user;
 
 defineProps({
     posts: Object
@@ -13,13 +16,18 @@ defineProps({
 
 <template>
     <Head title="Home"/>
-
+    <AuthenticatedLayout>
     <div class="grid grid-cols-12 gap-3">
         <div class="col-span-3">
             <CommunityList/>
         </div>
 
-        <div class="col-span-6">
+        <div>
+            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+            </div>
+        </div>
+
+        <div class="col-span-5">
             <CreatePost/>
             <PostList :posts="posts.data"/>
         </div>
@@ -28,4 +36,6 @@ defineProps({
             <SearchList/>
         </div>
     </div>
+    </AuthenticatedLayout>
+    
 </template>
--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -5,9 +5,28 @@ const user = usePage().props.auth.user;
 </script>
 
 <template>
-    <div>
-        <pre>{{ user }}</pre>
+    <a href="/dashboard">
+        <button
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        Back
+        </button>
+    </a>
+    <br>
+    <div class="relative">
+        <img src="https://static.vecteezy.com/system/resources/previews/021/548/095/original/default-profile-picture-avatar-user-avatar-icon-person-icon-head-icon-profile-picture-icons-default-anonymous-user-male-and-female-businessman-photo-placeholder-social-network-avatar-portrait-free-vector.jpg"
+            class="absolute-left-[100px] w-[128px] h-[128px] b-0"> 
+        <br>
+        <pre>Name   : {{ user.name }}</pre>
+        <pre>Email  : {{ user.email }}</pre>
     </div>
+    <br>
+    <a href="/profile">
+        <button
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        Edit Profile 
+        </button>
+    </a>
+    
 </template>
 
 <style lang="scss" scoped>
